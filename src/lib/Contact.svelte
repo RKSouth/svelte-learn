52ac05d5f08f5880ed76b0f4c3ea23741cf1c4c8
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2023-01-25 18:56:44 -0800

    fixed spacing

---
+++ src/lib/Contact.svelte
@@ -0,0 +1,14 @@
+<ul>
+    <li><strong>Email:</strong>rkelmsouthworth@gmail.com</li>
+    <li><strong>LinkedIn:</strong>  <a href="https://www.linkedin.com/in/rachael-kelm-southworth-87a3831b3/">  www.linkedin.com/in/rachael-kelm-southworth-87a3831b3</a></li>
+    <li><strong>Github:</strong><a href="https://github.com/RKSouth">  https://github.com/RKSouth</a> </li>
+</ul>
+
+<style>
+ul {
+    padding: 1.5rem;
+}
+li {
+    padding: 0.5rem;
+}
+</style>
